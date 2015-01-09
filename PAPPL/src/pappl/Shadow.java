9d708d830e8d503385255d30af8c0513f07e0bd5
avinesse <avinesse@pc-b13-10.eleves.ecn>
2015-01-09 14:11:34 +0100

    fix createShadow nb de pts

--- PAPPL/src/pappl/Shadow.java
+++ PAPPL/src/pappl/Shadow.java
@@ -13,7 +13,6 @@ import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
 
-
 /**
  *
  * @author avinesse
@@ -36,23 +35,24 @@ public class Shadow {
         this.altitude = altitude;
         this.azimuth = azimuth;
     }
-/**
- * Constructeur
- * @param sun1 : position du soleil
- * @param lat : latitude
- * @param lon : longitude
- */
+
+    /**
+     * Constructeur
+     *
+     * @param sun1 : position du soleil
+     * @param lat : latitude
+     * @param lon : longitude
+     */
     public Shadow(SunPosition sun1, double lat, double lon) {
         this.sun = sun1;
-        this.altitude = SunPosition.getPosition(sun1.getDate(),lat,lon).get(0);
-        this.azimuth = SunPosition.getPosition(sun1.getDate(),lat,lon).get(1);
-        
+        this.altitude = SunPosition.getPosition(sun1.getDate(), lat, lon).get(0);
+        this.azimuth = SunPosition.getPosition(sun1.getDate(), lat, lon).get(1);
+
     }
-    
-    
 
     /**
      * On calcule la direction du soleil
+     *
      * @param altitude
      * @param azimuth
      * @return les coordonnées du soleil
@@ -65,13 +65,16 @@ public class Shadow {
 
         return direction;
     }
-/**
- * Calcul des coordonnées de l'ombre d'un point 2D avec une hauteur en fonction de la position du soleil
- * @param c : coordonnées du point dont il faut calculer l'ombre
- * @param h : hauteur de ce point
- * @param direction : direction du soleil
- * @return : les coordonnées calculées du point de l'ombre
- */
+
+    /**
+     * Calcul des coordonnées de l'ombre d'un point 2D avec une hauteur en
+     * fonction de la position du soleil
+     *
+     * @param c : coordonnées du point dont il faut calculer l'ombre
+     * @param h : hauteur de ce point
+     * @param direction : direction du soleil
+     * @return : les coordonnées calculées du point de l'ombre
+     */
     public Coordinate projection(Coordinate c, double h, Coordinate direction) {
         Coordinate project = new Coordinate();
         // calcul des coordonnées
@@ -79,22 +82,22 @@ public class Shadow {
         project.y = c.y + direction.y * h;
         return project;
     }
-/**
- * Calcul de l'ombre pour une base toute entière
- * @param base dont il faut calculer l'ombre
- * @param height : hauteur de la base
- * @param direction du soleil
- */
+
+    /**
+     * Calcul de l'ombre pour une base toute entière
+     *
+     * @param base dont il faut calculer l'ombre
+     * @param height : hauteur de la base
+     * @param direction du soleil
+     */
     public Polygon createShadow(Polygon polygon, double height, Coordinate direction) {
-        
+
         GeometryFactory factory = polygon.getFactory();
         final LineString shell = polygon.getExteriorRing();
         ArrayList<Coordinate> shadowPoints = new ArrayList<>();
-        
+
         Coordinate a = new Coordinate(0, 0);
         Coordinate b = new Coordinate(0, 0);
-        //Coordinate _a = new Coordinate(0, 0);
-        //Coordinate _b = new Coordinate(0, 0);
 
         for (int i = 0; i < shell.getNumPoints() - 1; i++) {
             a.x = shell.getCoordinateN(i).x - ORIGIN_X;
@@ -102,22 +105,17 @@ public class Shadow {
             b.x = shell.getCoordinateN(i + 1).x - ORIGIN_X;
             b.y = shell.getCoordinateN(i + 1).y - ORIGIN_Y;
 
-            /*_a.x = projection(a, height, direction).x;
-            _a.y = projection(a, height, direction).y;
-            _b.x = projection(b, height, direction).x;
-            _b.y = projection(b, height, direction).y;*/
 
 
-
-            shadowPoints.add(new Coordinate(projection(a, height, direction).x,projection(a, height, direction).y));
-            shadowPoints.add(new Coordinate(projection(b, height, direction).x,projection(b, height, direction).y));
+            shadowPoints.add(new Coordinate(projection(a, height, direction).x, projection(a, height, direction).y));
+            if (i == shell.getNumPoints() - 2) {
+                shadowPoints.add(new Coordinate(projection(b, height, direction).x, projection(b, height, direction).y));
+            }
 
 
         }
-        
-        //CoordinateArraySequence shadowOutline = new CoordinateArraySequence(shadowPoints.toArray(new Coordinate[shadowPoints.size()]));
-        //LineString shadow = new LineString(shadowOutline,factory);
-        Polygon shadowBuilding = factory.createPolygon(new LinearRing(new CoordinateArraySequence(shadowPoints.toArray(new Coordinate[shadowPoints.size()])),factory), null);
+
+        Polygon shadowBuilding = factory.createPolygon(new LinearRing(new CoordinateArraySequence(shadowPoints.toArray(new Coordinate[shadowPoints.size()])), factory), null);
         return shadowBuilding;
     }
 }
